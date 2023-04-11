b3b275c75b4c9e1fa09d3e43cc71e3c227cd953f
Ivan <foxriver660@gmail.com>
2023-04-11 19:34:40 +0300

    initial commit

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -11,7 +11,7 @@ const fib = (n: number) => {
   for (let i = 2; i < n + 1; i++) {
     arr.push(arr[i - 2] + arr[i - 1]);
   }
-  return arr;
+  return arr; 
 };
 
 export const FibonacciPage: React.FC = () => {
