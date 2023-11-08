f0937e15108ad1a7e7e615c201a67cf86b80728d
foxriver <foxriver660@gmail.com>
2023-11-08 13:01:34 +0300

    update route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,7 +12,7 @@ import "./app.css";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/visualized-algorithms/">
         <Switch>
           <Route path="/" exact>
             <MainPage />
